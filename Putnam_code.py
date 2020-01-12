25122f9c9a3d2ec919364e3a55ec410aa0286480
DavidKu <ts01561526@gmail.com>
2020-01-12 17:30:33 -0500

    update code

--- Putnam_code.py
+++ Putnam_code.py
@@ -1,7 +1,17 @@
 #%% import packages
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import re
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+import sklearn
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
 
 #%% import data
 map_directory = "/Users/dauku/Desktop/career/Cover Letter/Putnam/data/partnership_map.csv"
@@ -102,6 +112,135 @@ touchpoint_individual = touchpoint.groupby(["ID"]).agg({'EMAIL_SENT':'sum','EMAI
 # merge touchpoint information onto trades table
 all_table = pd.merge(trades_agg, touchpoint_individual, how = "right", left_index = True, right_index = True)
 all_table = pd.merge(firm_info, all_table, left_on = "ID", right_index = True)
-all_table.set_index("ID", inplace = True)
+
+# Takign out the outlier
+all_table_nooutlier = all_table[all_table["ID"] != "ZEKBKU4CMP"]
+
+# export the data
+all_table.to_csv("/Users/dauku/Desktop/career/Cover Letter/Putnam/data/all_table.csv")
 
 #%% visualization
+# high value traders
+# sns.set(style="whitegrid")
+# # Initialize the matplotlib figure
+# f, ax = plt.subplots(figsize=(6, 10))
+#
+# # sns.set_color_codes("pastel")
+# barchart = all_table.sort_values(by = "TRAN_AMT", ascending = False).iloc[:21, :]
+# sns.set_color_codes("muted")
+# sns.barplot(x="TRAN_AMT", y="ID", data=barchart, color="b")
+# ax.set_title("Top 20 high value traders", )
+# plt.show()
+
+# pair scatter plot to explore the relationshiop between variables
+sns.pairplot(all_table)
+plt.show()
+
+sns.pairplot(all_table, hue = "ELITE_FIRM")
+plt.show()
+
+sns.pairplot(all_table, hue = "FIRM_NAME")
+plt.show()
+
+# pair scatter plot without outlier
+sns.pairplot(all_table_nooutlier)
+plt.show()
+#%% Modeling
+# one-hot encoding categorical variables
+encoded = pd.get_dummies(all_table, prefix_sep = "", columns = ["FIRM_NAME", "ELITE_FIRM"], prefix = ["", "elite"])
+
+x = encoded.drop(["TRAN_AMT", "ID"], axis = 1)
+y = encoded[["TRAN_AMT"]]
+
+# Lasso Regression used for variable selection
+lasso = Lasso(tol = 0.5)
+
+parameters = {"alpha": [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10, 20, 50, 100]}
+
+lasso_regressor = GridSearchCV(lasso, parameters, scoring = "neg_mean_squared_error", cv = 4)
+
+lasso_regressor.fit(x, y)
+
+print(lasso_regressor.best_params_)
+print(lasso_regressor.best_score_)
+
+lasso_regression = Lasso(alpha = 100, tol = 0.5)
+lasso_regression.fit(x, y)
+
+print(lasso_regression.score(x, y))
+print(lasso_regression.coef_)  # this shows that all the variable are significant
+
+# Moving to linear regression to get Beta coefficients
+lin_reg = LinearRegression()
+
+# mean_MSE = np.mean(MSE)
+# print(mean_MSE)
+
+reg = lin_reg.fit(x, y)
+reg.score(x, y)
+reg.coef_
+
+# without outlier
+# no_outlier = encoded[encoded["ID"] != "ZEKBKU4CMP"]
+#
+# x1 = no_outlier.drop(["TRAN_AMT", "ID"], axis = 1)
+# y1 = no_outlier[["TRAN_AMT"]]
+#
+# lin_reg = LinearRegression()
+#
+# MSE = cross_val_score(lin_reg, x1, y1, cv = 4)
+#
+# mean_MSE = np.mean(MSE)
+# print(mean_MSE)
+#
+# reg = lin_reg.fit(x1, y1)
+# reg.score(x1, y1)
+
+#%% Clustering (here we will perfrom the clustering after removing outliers)
+# Standardize the data
+scaler = StandardScaler()
+
+clustering_data = all_table_nooutlier.set_index("ID")
+clustering_data.drop(["FIRM_NAME", "ELITE_FIRM"], axis = 1, inplace = True)  # only cluter based on continuous variables
+
+scaler.fit(clustering_data)
+Standardized = scaler.transform(clustering_data)
+
+Standardized = pd.DataFrame(Standardized)
+Standardized.columns = clustering_data.columns
+Standardized.index = clustering_data.index
+
+# performing PCA
+pca = PCA(n_components = 2)
+PCAs = pd.DataFrame(pca.fit_transform(Standardized))
+PCAs.columns = ["PC1", "PC2"]
+
+# project data onto two PCs
+fig, ax = plt.subplots(1, 1)
+fig.set_figheight(10)
+fig.set_figwidth(10)
+ax.set_xlabel("PC1", fontsize = 15)
+ax.set_ylabel("PC2", fontsize = 15)
+ax.set_title("2 Principle Components", fontsize = 20)
+
+ax.scatter(PCAs.iloc[:, 0], PCAs.iloc[:, 1])
+plt.show()
+
+# Clustering
+dbscan = DBSCAN(eps = 0.5, min_samples = 6)
+clusters = pd.DataFrame(dbscan.fit_predict(PCAs))
+
+PCAs["clusters"] = clusters
+
+fig, ax = plt.subplots(1, 1)
+fig.set_figheight(10)
+fig.set_figwidth(10)
+ax.set_xlabel("PC1", fontsize = 15)
+ax.set_ylabel("PC2", fontsize = 15)
+ax.set_title("2 Principle Components with clustering", fontsize = 20)
+ax.legend(clusters)
+
+sns.lmplot(data = clusters, x = "PC1", y = "PC2", hue = "clusters", fit_reg = False, legend = True, legend_out = True)
+# ax.scatter(PCAs.iloc[:, 0], PCAs.iloc[:, 1], c = clusters, cmap = "plasma")ZZ
+plt.show()
+
